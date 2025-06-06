0302380d305678f8ec296fd5e6dd7bbd6d3fec78
coco <coco@gmail.com>
2025-06-06 11:49:20 -0500

    segundo commit

--- src/main/java/Dao/ClienteImp.java
+++ src/main/java/Dao/ClienteImp.java
@@ -106,8 +106,28 @@ public class ClienteImp implements ICliente {
 	}  //fin del metodo listar....
 
 	public TblCliente BuscarCliente(TblCliente cliente) {
-		// TODO Auto-generated method stub
-		return null;
+		//nos conectamos con la u.p.
+				EntityManagerFactory emf=Persistence.createEntityManagerFactory("ProyectoJPAWebMiercoles");
+				//para administrar las transacciones
+				EntityManager em=emf.createEntityManager();
+				//para buscar
+				TblCliente buscarcodigo=null;
+				try{
+					//iniciamos la transaccion..
+					em.getTransaction().begin();
+					//recuperamos el codigo a buscar...
+					buscarcodigo=em.find(TblCliente.class,cliente.getIdcliente());
+				     //confirmamos
+					em.getTransaction().commit();
+				
+				}catch(RuntimeException e){
+					System.out.println(e.getMessage());
+				}finally{
+					//cerramos
+					em.close();
+				}  //fin del finally
+				//retornamos el listado
+				return buscarcodigo;
 	}  //fin del metodo buscar....
 
 } //fin de la clase...
